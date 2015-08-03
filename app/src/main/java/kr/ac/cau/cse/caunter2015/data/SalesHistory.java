4f51b2d5303fcbf85007b08c9d95ac4ba52f2ac1
Indigobit <cho_hol@outlook.com>
2015-08-03 16:15:57 +0900

    add getter

--- app/src/main/java/kr/ac/cau/cse/caunter2015/data/SalesHistory.java
+++ app/src/main/java/kr/ac/cau/cse/caunter2015/data/SalesHistory.java
@@ -9,16 +9,4 @@ public class SalesHistory {
     private int id;
     private ArrayList<HashMap<Product, Integer>> salesList;
     private GregorianCalendar date;
-
-    public int getId() {
-        return id;
-    }
-
-    public ArrayList<HashMap<Product, Integer>> getSalesList() {
-        return salesList;
-    }
-
-    public GregorianCalendar getDate() {
-        return date;
-    }
 }
