e563629904b2613ca9d23bee9dbd1aacc924b786
kameru <miri1208@naver.com>
2015-08-03 16:02:20 +0900

    add Deal directory and simple model

---
+++ app/src/main/java/kr/ac/cau/cse/caunter2015/Deal/DealModel.java
@@ -0,0 +1,57 @@
+package kr.ac.cau.cse.caunter2015.Deal;
+
+public class DealModel {
+    private int productId;
+    private String productName;
+    private int categoryId;
+    private int price;
+    private int startStock;
+    private int stock;
+    private int totalPrice;
+
+    public DealModel(int productId, String productName, int categoryId, int price, int startStock, int stock, int totalPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.categoryId = categoryId;
+        this.price = price;
+        this.startStock = startStock;
+        this.stock = stock;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStartStock() {
+        return startStock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
