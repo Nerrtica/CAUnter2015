f126fb0c0961422c592fd99f5f11d7f380625e25
Indigobit <cho_hol@outlook.com>
2015-08-12 17:25:04 +0900

    change DBManager import location

--- app/src/main/java/kr/ac/cau/cse/caunter2015/MainActivity.java
+++ app/src/main/java/kr/ac/cau/cse/caunter2015/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import kr.ac.cau.cse.caunter2015.database.DBManager;
 
 public class MainActivity extends Activity {
 
