9b7f5dcd2ba9122f21e064e2e288430adf7a18bb
kameru <kameru1208@gmail.com>
2015-10-31 15:44:57 +0900

    get intent from event display

--- app/src/main/java/kr/ac/cau/cse/caunter2015/dealActivity/DealActivity.java
+++ app/src/main/java/kr/ac/cau/cse/caunter2015/dealActivity/DealActivity.java
@@ -1,23 +1,39 @@
 package kr.ac.cau.cse.caunter2015.dealActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+
 import kr.ac.cau.cse.caunter2015.R;
+import kr.ac.cau.cse.caunter2015.data.Product;
+import kr.ac.cau.cse.caunter2015.database.DBManager;
 
 public class DealActivity extends Activity implements View.OnClickListener {
-    private int totalPrice;
+    private int totalPrice = 0;
+    private DBManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deal);
-        totalPrice = 0;
 
-    }
+        dbManager = new DBManager(this);
+        ArrayList<Product> productList = new ArrayList<>();
+
+        //get data from event select activity
+        Intent intentEvent = getIntent();
+        int eventId = intentEvent.getIntExtra("eventId",0);
+
+        productList = dbManager.selectProduct(eventId);
+
+        createButton(productList);
+
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,5 +59,26 @@ public class DealActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+    }
+
+
+    private void createButton(final ArrayList<Product> productList) {
+    //  Make button List
+        /*
+        int size = productList.size();
+        final Button btnList[] = new Button[size];
+
+        for(int i=0;i<size;i++){
+            btnList[i]=new Button(this);
+            btnList[i].setWidth(150);
+            btnList[i].setHeight(150);
+            btnList[i].setText(productList.get(i).getName()+"\n"+productList.get(i).getPrice()+"\n"+productList.get(i).getInitialStock());
+            btnList[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
+        }
+        */
     }
 }
